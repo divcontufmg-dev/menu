a1a0dfdc1167b781f71b192b748001e6f1e0c871
divcontufmg-dev <divcont.ufmg@gmail.com>
2026-02-19 11:06:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ st.title("⚙️ Menu Central de Ferramentas")
 st.write("Bem-vindo! Selecione abaixo a automação que deseja utilizar:")
 
 # Criando botões grandes que levam para as outras páginas
-st.page_link("vbarmb.py", label="📊 1. VBA RMB", icon="▶️")
-st.page_link("vbadep.py", label="🔧 2. VBA Depreciação", icon="▶️")
+st.page_link("pages/vbarmb.py", label="📊 1. VBA RMB", icon="▶️")
+st.page_link("pages/vbadep.py", label="🔧 2. VBA Depreciação", icon="▶️")
 st.page_link("pages/rmb.py", label="📝 3. Conciliador RMB x SIAFI", icon="▶️")
-st.page_link("dep.py", label="📁 4. Conciliador Depreciação x SIAFI", icon="▶️")
+st.page_link("pages/dep.py", label="📁 4. Conciliador Depreciação x SIAFI", icon="▶️")
 
 st.divider()
 st.info("💡 **Dica:** Você também pode usar o menu lateral esquerdo para navegar entre as ferramentas e voltar para esta tela inicial a qualquer momento.")
