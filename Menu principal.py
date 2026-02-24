e506c754c508a91a85825a55ea8ffa49ac249622
divcontufmg-dev <divcont.ufmg@gmail.com>
2026-02-24 15:36:30 -0300

    Update Menu principal.py

--- Menu principal.py
+++ Menu principal.py
@@ -7,10 +7,10 @@ st.title("⚙️ Menu Central de Ferramentas")
 st.write("Bem-vindo! Selecione abaixo a automação que deseja utilizar:")
 
 # Criando botões grandes que levam para as outras páginas
-st.page_link("pages/1_Preparar planilha Siafi RMB.py", label="📊 1. VBA RMB", icon="▶️")
-st.page_link("pages/2_Conciliador_RMB_x_SIAFI.py", label="📝 2. Conciliador RMB x SIAFI", icon="▶️")
-st.page_link("pages/3_Preparar planilha Siafi Depreciação.py", label="🔧 3. VBA Depreciação", icon="▶️")
-st.page_link("pages/4_Conciliador_Depreciação_x_SIAFI.py", label="📁 4. Conciliador Depreciação x SIAFI", icon="▶️")
+# st.page_link("pages/1_Preparar planilha Siafi RMB.py", label="📊 1. VBA RMB", icon="▶️")
+st.page_link("pages/2.1_Conciliador_RMB_x_SIAFI.py", label="📝 2. Conciliador RMB x SIAFI", icon="▶️")
+# st.page_link("pages/3_Preparar planilha Siafi Depreciação.py", label="🔧 3. VBA Depreciação", icon="▶️")
+st.page_link("pages/4.1_Conciliador_Depreciação_x_SIAFI.py", label="📁 4. Conciliador Depreciação x SIAFI", icon="▶️")
 st.page_link("pages/5_Conciliador_Almoxarifado_x_SIAFI.py", label="💼 5. Conciliador Almoxarifado x SIAFI", icon="▶️")
 
 st.divider()
