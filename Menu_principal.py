c433fe0e9010d9f5da1e91573f524d521194d11a
divcontufmg-dev <divcont.ufmg@gmail.com>
2026-08-03 15:26:49 -0300

    Update Menu_principal.py

--- Menu_principal.py
+++ Menu_principal.py
@@ -37,5 +37,6 @@ st.page_link("pages/4.1_Conciliador_Depreciação_x_SIAFI.py", label="📁 2. Co
 st.page_link("pages/5.1_Conciliador_Almoxarifado_x_SIAFI.py", label="💼 3. Conciliador Almoxarifado x SIAFI", icon="▶️")
 st.page_link("pages/6_Conciliador_Biblioteca.py", label="💻 4. Conciliador Biblioteca", icon="▶️")
 st.page_link("pages/7_Gerador_mapa_restrição.py", label="💻 5. Gerador de mapa de restrição contábil", icon="▶️")
+st.page_link("pages/teste.py", label="💻 tde mapa de restrição contábil", icon="▶️")
 
 #st.page_link("pages/testrereav.py", label="💻 test", icon="▶️")
