ce5d4ff661ebc4122f694019adeb8a8f46411beb
divcontufmg-dev <divcont.ufmg@gmail.com>
2026-08-13 10:52:03 -0300

    Update Menu_principal.py

--- Menu_principal.py
+++ Menu_principal.py
@@ -30,10 +30,10 @@ st.markdown("---")
 
 # Criando botões grandes que levam para as outras páginas
 # st.page_link("pages/1_Preparar planilha Siafi RMB.py", label="📊 1. VBA RMB", icon="▶️")
-st.page_link("pages/2.1r_Conciliador_RMB_x_SIAFI.py", label="📝 1. Conciliador RMB x SIAFI", icon="▶️")
+st.page_link("pages/2.1_Conciliador_RMB_x_SIAFI.py", label="📝 1. Conciliador RMB x SIAFI", icon="▶️")
 # st.page_link("pages/3_Preparar planilha Siafi Depreciação.py", label="🔧 3. VBA Depreciação", icon="▶️")
 
-st.page_link("pages/4.1_Conciliador_Depreciação_x_SIAFI.py", label="📁 2. Conciliador Depreciação x SIAFI", icon="▶️")
+st.page_link("pages/4.1r_Conciliador_Depreciação_x_SIAFI.py", label="📁 2. Conciliador Depreciação x SIAFI", icon="▶️")
 st.page_link("pages/5.1_Conciliador_Almoxarifado_x_SIAFI.py", label="💼 3. Conciliador Almoxarifado x SIAFI", icon="▶️")
 #st.page_link("pages/6r_Conciliador_Biblioteca.py", label="💻 4. Conciliador Biblioteca teste", icon="▶️")
 st.page_link("pages/6_Conciliador_Biblioteca.py", label="💻 4. Conciliador Biblioteca", icon="▶️")
